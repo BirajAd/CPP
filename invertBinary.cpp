d8ab3ff1aabc7c87e324711c23298637c749dd90
Biraj Adhikari <adhikari.biraj1@gmail.com>
2021-12-28 23:11:01 -0600

    order traversal close to done

--- invertBinary.cpp
+++ invertBinary.cpp
@@ -14,32 +14,70 @@ struct TreeNode {
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
-    public:
-        struct TreeNode* invertTree(TreeNode* root) {
-            queue<TreeNode> q;
-            q.push(*root);
-            if(root == NULL){
-                cout << "null";
-                return NULL;
-            } else {
-                cout << root->val << endl;
-                return &q.front();
-            }
-            // while(!q.empty())
-            // {
-            //     TreeNode *head = q.pop();
-            //     q.push(head.left);
-            //     q.push(head.right);
-            //     TreeNode temp = head.left;
-            //     head.left = head.right;
-            //     head.right = temp;
-            // }
-        };
-        int main()
+
+void invertTree(TreeNode* root) {
+    queue<TreeNode*> q;
+    q.push(root);
+    if(root == NULL){
+        cout << "null";
+    } else {
+        while(!q.empty())
         {
-            TreeNode a = TreeNode();
-            invertTree(&a);
-            return 0;
+            if(root->left != NULL) q.push(root->left);
+            if(root->right != NULL) q.push(root->right); 
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            cout << root->left->val << " and " << root->right->val << endl;
+            q.pop();
+            root = q.front();
+            cout<< (root->right!=NULL ? root->right->val : -999) << endl;
+            cout << "empty: " << q.front()->val << endl;
         };
+        cout << "end here" << endl;
+    }
+
+};
+
+void orderTraversal(TreeNode* n)
+{
+    queue<TreeNode*> q;
+    TreeNode* root = n;
+    if(root == NULL)
+    {
+        cout << "root is null" << endl;
+    }
+    q.push(root);
+    while(!q.empty())
+    {
+        TreeNode* curr = q.front();
+        q.pop();
+        cout << curr->val << ",";
+        if(curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if(curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+}
+
+int main()
+{
+    TreeNode* a = new TreeNode(9);
+    a->left = new TreeNode(4);
+    a->right = new TreeNode(15);
+    a->left->left = new TreeNode(2);
+    a->left->right = new TreeNode(5);
+    a->right->left = new TreeNode(10);
+    a->right->right = new TreeNode(18);
+    orderTraversal(a);
+    cout << endl;
+    invertTree(a);
+    cout << a->val << "ended";
+    cout << "done";
+
+    return 0;
 };
