a221be6457c6012dc7fda61e6f93e02ee064d056
BirajAd <adhikari.biraj1@gmail.com>
2022-02-18 09:36:42 -0600

    missing number finding for cses

---
+++ missing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    long long int n, sum=0, a;
+    cin >> n;
+    for(int i=0; i<n-1; i++) {
+        cin >> a;
+        sum += a;
+    }
+    cout << n*(n+1)/2 - sum;
+    return 0;
+}
