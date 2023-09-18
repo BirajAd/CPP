c6d6d1a9ac5fb696d6849a97a4978e48b95bac70
BirajAd <adhikari.biraj1@gmail.com>
2023-09-18 07:13:58 -0500

    increasing array solved

--- problems/increasingArray.cpp
+++ problems/increasingArray.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
-
+#define ll long long
 using namespace std;
 
 int increasingArray(vector<int> &nums) {
@@ -9,12 +9,25 @@ int increasingArray(vector<int> &nums) {
     for(int i=1; i<nums.size(); i++){
         if(nums[i-1] > nums[i]) {
             steps += (nums[i-1]-nums[i]);
+            nums[i] = nums[i-1];
         }
     }
     return steps;
 }
 
 int main(){
-    vector<int> all = { 3, 2, 5, 1, 7 };
-    cout << increasingArray(all) << "\n";
+    // vector<int> all = { 3, 2, 5, 1, 7 };
+    // cout << increasingArray(all) << "\n";
+    ll int n;
+    cin >> n;
+    ll int nums[n];
+    ll int totalSteps = 0;
+    for(ll int i=0; i<n; i++) {
+        cin >> nums[i];
+        if(i > 0 && nums[i] < nums[i-1]) {
+            totalSteps += (nums[i-1]-nums[i]);
+            nums[i] = nums[i-1];
+        }
+    }
+    cout << totalSteps << "\n";
 }
