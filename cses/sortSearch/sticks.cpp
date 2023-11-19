1e4e713f493de885355dc14ce91659f01f6dbe61
BirajAd <adhikari.biraj1@gmail.com>
2023-11-19 15:14:38 -0600

    sticks solution

--- cses/sortSearch/sticks.cpp
+++ cses/sortSearch/sticks.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <limits.h>
+#include <algorithm>
 #include <cmath>
 
 #define ll long long
@@ -8,27 +8,26 @@
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-	cin.tie(nullptr);
 
-	freopen("input.in", "r", stdin);
-	freopen("output.out", "w", stdout);
+  // ios::sync_with_stdio(false);
+	// cin.tie(nullptr);
+
+	// freopen("input.in", "r", stdin);
+	// freopen("output.out", "w", stdout);
+
   int n;
   cin >> n;
   vector<ll int> v(n);
-  ll int sum = 0;
-  ll int x;
-  for(int i=0; i<n; i++) {
-    cin >> x;
-    v[i] = x;
-    sum += x;
+  for(ll int &i: v) {
+    cin >> i;
   }
-  // sort(v.begin(), v.end());
-  ll int avg = round((double)sum/n);
-  cout << avg << "\n";
+  sort(v.begin(), v.end());
+
+  ll int mdn = v[n/2];
+
   ll int ans = 0;
   for(auto x: v) {
-    ans += abs(avg-x);
+    ans += abs(mdn-x);
   }
   cout << ans << "\n";
 }
