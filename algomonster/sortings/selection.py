ef256cbc2e72a4d5445ba2c49f25406531fe8e4b
Biraj Ad <adhikari.biraj1@gmail.com>
2025-11-07 00:01:39 -0600

    bubble and selection completed

--- algomonster/sortings/selection.py
+++ algomonster/sortings/selection.py
@@ -13,7 +13,34 @@ def insertion_sort(arr):
   return arr
 
 arr = [5,2,1,3,4]
-print(insertion_sort(arr))
+# print(insertion_sort(arr))
 
+# 5 3 1 2 4
+def selection_sort(arr):
+  n = len(arr)
+  for i in range(n):
+    minn = arr[i]; min_idx = i
+    for j in range(i+1, n):
+      if arr[j] < minn:
+        minn = arr[j]; min_idx = j
+    print(arr)
+    arr[min_idx], arr[i] = arr[i], arr[min_idx]
+  return arr
+
+# 5 3 1 2 4
 def bubble_sort(arr):
-  return NotImplemented
+  n = len(arr)
+  for i in range(n):
+    swapped = False
+    for j in range(1, n):
+      if arr[j-1] > arr[j]:
+        arr[j-1], arr[j] = arr[j], arr[j-1]
+        swapped = True
+    if not swapped: return arr
+  
+  return arr
+
+
+
+inp = input("copy paste your list ")
+bubble_sort(list(map(int, inp.split(","))))
